4bb50e08776542eb786305b4a2592dc88dfb8a27
MD-DILDAR-MANDAL <93587947+MD-DILDAR-MANDAL@users.noreply.github.com>
2022-01-19 16:34:36 +0530

    just push

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//sum to nth term 
 int sum(int n){
     if(n == 0) return 0;//base
     int temp = sum(n-1);
     return temp+n;
     }
 
+//factorial
 int facto(int m){
     if(m == 0) return 1; //base
     return facto(m-1)*m;
