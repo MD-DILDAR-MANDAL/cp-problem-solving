7d5be53bfc398ae7a65d456a503d8ccc1915b999
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2024-09-07 14:05:34 +0530

    gfg: Find pairs with given sum in doubly linked list

---
+++ Linked_list/findPairsWithGivenSum.cpp
@@ -0,0 +1,107 @@
+//https://www.geeksforgeeks.org/problems/find-pairs-with-given-sum-in-doubly-linked-list/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=find-pairs-with-given-sum-in-doubly-linked-list
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next, *prev;
+    Node(int val) : data(val), next(NULL), prev(NULL)
+    {
+    }
+};
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/* Doubly linked list node class
+class Node
+{
+public:
+    int data;
+    Node *next, *prev;
+    Node(int val) : data(val), next(NULL), prev(NULL)
+    {
+    }
+};
+*/
+
+class Solution
+{
+public:
+    vector<pair<int, int>> findPairsWithGivenSum(Node *head, int target)
+    {
+        vector<pair<int,int>>vp;
+        pair<int,int>pr;
+        Node* left = head;
+        Node* right = head;
+        
+        while(right->next != NULL){
+            right=right->next;
+        }
+        int sum=0;
+        while(left->data < right->data){
+            sum = left->data + right->data;
+            
+            if(sum < target){
+                left = left->next;
+            }
+            else if(sum == target){
+                vp.push_back(make_pair(left->data,right->data));
+                left = left->next;
+                right = right->prev;
+
+            }
+            else{
+                right=right->prev;
+            }
+            
+        }
+        return vp;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, target;
+        cin >> target >> n;
+        int a;
+        cin >> a;
+        Node *head = new Node(a);
+        Node *tail = head;
+        for (int i = 0; i < n - 1; i++)
+        {
+            cin >> a;
+            tail->next = new Node(a);
+            tail->next->prev = tail;
+            tail = tail->next;
+        }
+        Solution ob;
+        auto ans = ob.findPairsWithGivenSum(head, target);
+        if (ans.size() == 0)
+            cout << "-1";
+        else
+        {
+            for (int i = 0; i < ans.size(); i++)
+            {
+                cout << "(" << ans[i].first << "," << ans[i].second << ")"
+                     << " ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
