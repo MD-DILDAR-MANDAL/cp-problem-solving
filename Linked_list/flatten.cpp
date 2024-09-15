1f88e707b079bc3c1ea649556815eef431c41b0b
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2024-09-15 23:55:26 +0530

    gfg: Flattening a Linked List

---
+++ Linked_list/flatten.cpp
@@ -0,0 +1,138 @@
+//https://www.geeksforgeeks.org/problems/flattening-a-linked-list/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=flattening-a-linked-list
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+    struct Node *bottom;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+        bottom = NULL;
+    }
+};
+
+void printList(Node *Node) {
+    while (Node != NULL) {
+        printf("%d ", Node->data);
+        Node = Node->bottom;
+    }
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+/* Node structure  used in the program
+
+struct Node{
+    int data;
+    struct Node * next;
+    struct Node * bottom;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+        bottom = NULL;
+    }
+
+};
+*/
+
+class Solution {
+  public:
+    // Function which returns the  root of the flattened linked list.
+    Node* sortDown(Node* head,Node* tmp){
+        if(tmp==NULL)return head;
+        
+        Node* tmp1=head;
+        Node* tmp2=tmp;
+        Node* dummy=new Node(-1);
+        Node* ans=dummy;
+        
+        while(tmp1!=NULL && tmp2 !=NULL){
+            if(tmp1->data > tmp2->data){
+                dummy->bottom=tmp2;
+                tmp2=tmp2->bottom;
+                
+            }
+            else{
+                dummy->bottom=tmp1;
+                tmp1=tmp1->bottom;
+
+            }
+            dummy=dummy->bottom;
+            
+            if(tmp1==NULL&&tmp2!=NULL){
+                dummy->bottom=tmp2;
+            }
+            else dummy->bottom=tmp1;
+        }
+        return sortDown(ans->bottom,tmp->next);
+        
+    }
+    
+    Node *flatten(Node *root) {
+        if(root==NULL && root->next==NULL)return NULL;
+        Node* head = root;
+        Node* tmp = head->next;
+        return sortDown(head,tmp);
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        vector<int> work;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            work.push_back(number);
+        }
+        n = work.size();
+
+        Node *head = NULL;
+        Node *pre = NULL;
+
+        for (int i = 0; i < n; i++) {
+            int m = work[i] - 1;
+            int data;
+            cin >> data;
+            Node *temp = new Node(data);
+            if (head == NULL) {
+                head = temp;
+                pre = temp;
+            } else {
+                pre->next = temp;
+                pre = temp;
+            }
+
+            Node *preB = temp;
+            for (int j = 0; j < m; j++) {
+                int temp_data;
+                cin >> temp_data;
+                Node *tempB = new Node(temp_data);
+                preB->bottom = tempB;
+                preB = tempB;
+            }
+        }
+
+        Solution ob;
+        Node *root = ob.flatten(head);
+        printList(root);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
