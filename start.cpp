3afdba454ef3d868b18950db8e93c74938e1213e
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2025-02-08 18:49:24 +0530

    leetCode: exponentiation using squaring

--- start.cpp
+++ start.cpp
@@ -1,31 +1,69 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 
-int main(){
-   int t;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
 
-   cin >> t; //test case
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
 
-   while(t--){
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
 
-      int x;
-      cin >>x; //12  12/10 q= 1 rem = 2;  1/10 q=0 rem= 1
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
 
-      int cnt = 0;
-      int num=x;
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
 
-      while(x>0){
-         int q = x/10;           //1 //0
-         int rem = x%10;         //2 //1
+void init_code()
+{
+#ifndef ONLINE_JUDGE
+   freopen("error.txt", "w", stderr);
+#endif
+}
 
-         if(num % rem == 0){         
-            cnt++;               //1
-         }
-         x = q;                  //x=1
+int main()
+{
+   init_code();
+   int n;
+   cin >>n;
 
-      }
-      cout << cnt << " ";
+
+   for(int i = 0; i<n; i++){
+     cout << i << " ";
    }
 
    return 0;
-}
+}  
