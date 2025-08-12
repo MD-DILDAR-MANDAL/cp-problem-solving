239218466b82cb8223f912c00460d9752ea9e3fe
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2025-08-12 09:33:37 +0530

    leetCode: merge intervals

--- amazonOA1.cpp
+++ amazonOA1.cpp
@@ -1,15 +1,18 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    void solve() {
+    void solve()
+    {
 
-        int len ;
+        int len;
         cin >> len;
-        vector<int>arr;
+        vector<int> arr;
         int x;
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < len; i++)
+        {
             cin >> x;
             arr.push_back(x);
         }
@@ -19,22 +22,28 @@ public:
         int first = arr[l];
         int second = arr[r];
         int diff = abs(first - second);
-        while (l < r && r < len - 1 ) {
+        while (l < r && r < len - 1)
+        {
             r++;
-            if (first > second) {
-                if (first > arr[r]) {
+            if (first > second)
+            {
+                if (first > arr[r])
+                {
                     first = arr[r];
                     diff = min(abs(first - second), diff);
                 }
             }
-            else {
-                if (second > arr[r]) {
+            else
+            {
+                if (second > arr[r])
+                {
                     second = arr[r];
                     diff = min(abs(first - second), diff);
                 }
             }
 
-            if (r == len - 1) {
+            if (r == len - 1)
+            {
                 ++l;
                 r = l + 1;
                 first = arr[l];
@@ -46,10 +55,12 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         Solution ans;
         ans.solve();
     }
--- start.cpp
+++ start.cpp
@@ -1,16 +1,18 @@
 #include<bits/stdc++.h>
+
 using namespace std;
-#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(null)
-#define mod 1000000007
-#define mod1 998244353
-#define inf 1e18
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
 #define nline "\n"
 #define pb push_back
 #define ppb pop_back
 #define mp make_pair
 #define ff first
 #define ss second
-#define pi 3.141592653589793238462
+#define PI 3.141592653589793238462
 #define set_bits __builtin_popcountll
 #define sz(x) ((int)(x).size())
 #define all(x) (x).begin(), (x).end()
@@ -18,74 +20,45 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
 
-#ifndef online_judge
+#ifndef ONLINE_JUDGE
 #define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
 #else
 #define debug(x)
 #endif
 
-void init_code() {
-#ifndef online_judge
-    freopen("error.txt", "w", stderr);
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void init_code()
+{
+#ifndef ONLINE_JUDGE
+	freopen("error.txt", "w", stderr);
 #endif
 }
 
-class solution {
-public:
-    void solve() {
-        int len ;
-        cin >> len;
-        vector<int>arr;
-        int x;
-        for (int i = 0; i < len; i++) {
-            cin >> x;
-            arr.push_back(x);
-        }
+int main()
+{
+	init_code();
 
-        unordered_map<int,int>freq;
-        int maxVal = INT_MIN;
-        int ans = 1;
+	
 
-        for(int i = 0;i<len;i++){
-            freq[arr[i]]++;
-            maxVal = max(arr[i],maxVal);
-        }
-
-        for (int i = 2; i <=maxVal*2; i++) {
-            int cnt = 0;
-            unordered_map<int, int>f=freq;
-
-            for(int j = 0;j<len;j++){
-                int x = arr[j];
-                if(f[x] > 0){
-                    int need = i - arr[j];
-                    if(x == need){
-                        if(f[x]>=2){
-                            f[x] -=2;
-                            cnt++;
-                        }
-                    }
-                    else if(f[need] >0){
-                        f[x]--;
-                        f[need]--;
-                        cnt++;
-                    }
-                }
-            }
-            ans = max(ans,cnt);
-        }
-        cout << ans << endl;
-    }
-};
-
-int main() {
-    init_code();
-    int t;
-    cin >> t;
-    while (t--) {
-        solution ans;
-        ans.solve();
-    }
-    return 0;
+	return 0;
 }
